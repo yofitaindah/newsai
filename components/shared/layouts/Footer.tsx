8537bf8ee58b5c892025b4c950f23ba6df7bf8f9
Yofita Indah <yofitaindah@gmail.com>
2025-01-05 13:40:40 +0700

    footer-change

--- components/shared/layouts/Footer.tsx
+++ components/shared/layouts/Footer.tsx
@@ -2,6 +2,13 @@ import Newsletter from "@/components/utils/Newsletter";
 import footerLogo from "@/public/images/logo_news.png";
 import Image from "next/image";
 import Link from "next/link";
+import DextoolIcon from '@/public/images/logo_dextool.svg';
+import EtherscanIcon from '@/public/images/logo_etherscan.svg';
+import TelegramIcon from '@/public/images/logo_telegram.svg';
+import TwitterIcon from '@/public/images/logo_twitter.svg';
+import YoutubeIcon from '@/public/images/logo_youtube.svg';
+import TiktokIcon from '@/public/images/logo_tiktok.svg';
+
 
 export default function Footer() {
 	return (
@@ -15,19 +22,19 @@ export default function Footer() {
 									<h6 className="text-white mb-2">News AI</h6>
 									<ul className="link-list list-unstyled mb-0">
 										<li>
-											<Link href="/about">About</Link>
+											<Link href="/about">NewsAI</Link>
 										</li>
 										<li>
-											<Link href="/blogs">Blog</Link>
+											<Link href="/contact">Tokenomics</Link>
 										</li>
 										<li>
-											<Link href="/login">Sign in</Link>
+											<Link href="/blogs">Roadmap</Link>
 										</li>
 										<li>
-											<Link href="/register">Register</Link>
+											<Link href="/use-cases">Team Details</Link>
 										</li>
 										<li>
-											<Link href="/contact">Contact</Link>
+											<Link href="/faq">FaQ</Link>
 										</li>
 									</ul>
 								</div>
@@ -38,102 +45,64 @@ export default function Footer() {
 									<Newsletter />
 									<ul className="list-unstyled d-flex flex-wrap align-center justify-center justify-md-start gap-3 social-list mb-0 mt-5">
 										<li>
-											<a href="#">
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													stroke="currentColor"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													strokeWidth="1.5"
-													viewBox="0 0 24 24"
-												>
-													<path stroke="none" d="M0 0h24v24H0z" />
-													<path d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21" />
-												</svg>
-											</a>
+											<Link href="/">
+												<Image
+													src={TelegramIcon}
+													alt="GenAi"
+													height={24}
+													priority
+												/>
+											</Link>
 										</li>
 										<li>
-											<a href="#">
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													stroke="currentColor"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													strokeWidth="1.5"
-													className="icon icon-tabler icon-tabler-brand-dribbble"
-													viewBox="0 0 24 24"
-												>
-													<path stroke="none" d="M0 0h24v24H0z" />
-													<circle cx="12" cy="12" r="9" />
-													<path d="M9 3.6c5 6 7 10.5 7.5 16.2" />
-													<path d="M6.4 19c3.5-3.5 6-6.5 14.5-6.4M3.1 10.75c5 0 9.814-.38 15.314-5" />
-												</svg>
-											</a>
+											<Link href="/">
+												<Image
+													src={TwitterIcon}
+													alt="GenAi"
+													height={24}
+													priority
+												/>
+											</Link>
 										</li>
 										<li>
-											<a href="#">
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 18 18"
-												>
-													<path
-														stroke="currentColor"
-														strokeLinecap="round"
-														strokeLinejoin="round"
-														strokeWidth="1.5"
-														d="M15.75 6v6A3.75 3.75 0 0 1 12 15.75H6A3.75 3.75 0 0 1 2.25 12V6A3.75 3.75 0 0 1 6 2.25h6A3.75 3.75 0 0 1 15.75 6Z"
-													/>
-													<path
-														stroke="currentColor"
-														strokeLinecap="round"
-														strokeLinejoin="round"
-														strokeWidth="1.5"
-														d="M8.25 15.75V9c0-1.641.375-3 3-3m-4.5 3.75h4.5"
-													/>
-												</svg>
-											</a>
+											<Link href="/">
+												<Image
+													src={YoutubeIcon}
+													alt="GenAi"
+													height={24}
+													priority
+												/>
+											</Link>
 										</li>
 										<li>
-											<a href="#">
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 18 18"
-												>
-													<path
-														stroke="currentColor"
-														strokeLinecap="round"
-														strokeLinejoin="round"
-														strokeWidth="1.5"
-														d="M17.25 2.258s-1.514.894-2.355 1.147A3.36 3.36 0 0 0 9 5.655v.75a7.995 7.995 0 0 1-6.75-3.397s-3 6.75 3.75 9.75a8.73 8.73 0 0 1-5.25 1.5c6.75 3.75 15 0 15-8.625a3.34 3.34 0 0 0-.06-.623c.765-.754 1.56-2.752 1.56-2.752Z"
-													/>
-												</svg>
-											</a>
+											<Link href="/">
+												<Image
+													src={TiktokIcon}
+													alt="GenAi"
+													height={24}
+													priority
+												/>
+											</Link>
 										</li>
 										<li>
-											<a href="#">
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 18 18"
-												>
-													<path
-														stroke="currentColor"
-														strokeLinecap="round"
-														strokeLinejoin="round"
-														strokeWidth="1.5"
-														d="M9 12a3 3 0 1 0 0-6 3 3 0 0 0 0 6v0Z"
-													/>
-													<path
-														stroke="currentColor"
-														strokeWidth="1.5"
-														d="M2.25 12V6A3.75 3.75 0 0 1 6 2.25h6A3.75 3.75 0 0 1 15.75 6v6A3.75 3.75 0 0 1 12 15.75H6A3.75 3.75 0 0 1 2.25 12Z"
-													/>
-												</svg>
-											</a>
+											<Link href="/">
+												<Image
+													src={DextoolIcon}
+													alt="GenAi"
+													height={24}
+													priority
+												/>
+											</Link>
+										</li>
+										<li>
+											<Link href="/">
+												<Image
+													src={EtherscanIcon}
+													alt="GenAi"
+													height={24}
+													priority
+												/>
+											</Link>
 										</li>
 									</ul>
 								</div>
