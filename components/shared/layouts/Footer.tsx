5bd3a13d2577634e99240971ff8468285f6b1b60
Yofita Indah <yofitaindah@gmail.com>
2025-01-05 13:50:09 +0700

    footer-change2

--- components/shared/layouts/Footer.tsx
+++ components/shared/layouts/Footer.tsx
@@ -9,107 +9,12 @@ import TwitterIcon from '@/public/images/logo_twitter.svg';
 import YoutubeIcon from '@/public/images/logo_youtube.svg';
 import TiktokIcon from '@/public/images/logo_tiktok.svg';
 
-
 export default function Footer() {
 	return (
 		<footer className="footer bg-striped pt-10 pt-lg-15">
 			<div className="container">
 				<div className="row g-10">
-					<div className="col-lg-9 col-xl-8 order-lg-2">
-						<div className="row g-6">
-							<div className="col-md-4 col-lg-4">
-								<div className="footer-widget text-center text-md-start">
-									<h6 className="text-white mb-2">News AI</h6>
-									<ul className="link-list list-unstyled mb-0">
-										<li>
-											<Link href="/about">NewsAI</Link>
-										</li>
-										<li>
-											<Link href="/contact">Tokenomics</Link>
-										</li>
-										<li>
-											<Link href="/blogs">Roadmap</Link>
-										</li>
-										<li>
-											<Link href="/use-cases">Team Details</Link>
-										</li>
-										<li>
-											<Link href="/faq">FaQ</Link>
-										</li>
-									</ul>
-								</div>
-							</div>
-							<div className="col-md-4 col-lg-4">
-								<div className="footer-widget text-center text-md-start">
-									<h6 className="text-white mb-4">News & Update</h6>
-									<Newsletter />
-									<ul className="list-unstyled d-flex flex-wrap align-center justify-center justify-md-start gap-3 social-list mb-0 mt-5">
-										<li>
-											<Link href="/">
-												<Image
-													src={TelegramIcon}
-													alt="GenAi"
-													height={24}
-													priority
-												/>
-											</Link>
-										</li>
-										<li>
-											<Link href="/">
-												<Image
-													src={TwitterIcon}
-													alt="GenAi"
-													height={24}
-													priority
-												/>
-											</Link>
-										</li>
-										<li>
-											<Link href="/">
-												<Image
-													src={YoutubeIcon}
-													alt="GenAi"
-													height={24}
-													priority
-												/>
-											</Link>
-										</li>
-										<li>
-											<Link href="/">
-												<Image
-													src={TiktokIcon}
-													alt="GenAi"
-													height={24}
-													priority
-												/>
-											</Link>
-										</li>
-										<li>
-											<Link href="/">
-												<Image
-													src={DextoolIcon}
-													alt="GenAi"
-													height={24}
-													priority
-												/>
-											</Link>
-										</li>
-										<li>
-											<Link href="/">
-												<Image
-													src={EtherscanIcon}
-													alt="GenAi"
-													height={24}
-													priority
-												/>
-											</Link>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 order-lg-1 me-auto">
+					<div className="col-lg-4">
 						<div className="footer-widget text-center text-lg-start">
 							<Link href="/">
 								<Image
@@ -125,8 +30,97 @@ export default function Footer() {
 							</p>
 						</div>
 					</div>
+					<div className="col-lg-4">
+						<div className="footer-widget text-center text-lg-start">
+							<h6 className="text-white mb-2">Quick Links</h6>
+							<ul className="link-list list-unstyled mb-0">
+								<li>
+									<Link href="/about">NewsAI</Link>
+								</li>
+								<li>
+									<Link href="/contact">Tokenomics</Link>
+								</li>
+								<li>
+									<Link href="/blogs">Roadmap</Link>
+								</li>
+								<li>
+									<Link href="/use-cases">Team Details</Link>
+								</li>
+								<li>
+									<Link href="/faq">FaQ</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="col-lg-4">
+						<div className="footer-widget text-center text-lg-start">
+							<h6 className="text-white mb-4">News & Update</h6>
+							<Newsletter />
+							<ul className="list-unstyled d-flex flex-wrap align-center justify-center justify-lg-start gap-3 social-list mb-0 mt-5">
+								<li>
+									<Link href="/">
+										<Image
+											src={TelegramIcon}
+											alt="GenAi"
+											height={24}
+											priority
+										/>
+									</Link>
+								</li>
+								<li>
+									<Link href="/">
+										<Image
+											src={TwitterIcon}
+											alt="GenAi"
+											height={24}
+											priority
+										/>
+									</Link>
+								</li>
+								<li>
+									<Link href="/">
+										<Image
+											src={YoutubeIcon}
+											alt="GenAi"
+											height={24}
+											priority
+										/>
+									</Link>
+								</li>
+								<li>
+									<Link href="/">
+										<Image
+											src={TiktokIcon}
+											alt="GenAi"
+											height={24}
+											priority
+										/>
+									</Link>
+								</li>
+								<li>
+									<Link href="/">
+										<Image
+											src={DextoolIcon}
+											alt="GenAi"
+											height={24}
+											priority
+										/>
+									</Link>
+								</li>
+								<li>
+									<Link href="/">
+										<Image
+											src={EtherscanIcon}
+											alt="GenAi"
+											height={24}
+											priority
+										/>
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
-
 				<div className="text-center py-6 mt-8">
 					<p className="fs-sm mb-0">
 						Copyright <span className="text-primary-dark">NewsAI</span>
