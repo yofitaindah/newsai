4d0179997a8fb3a838cbf48b2c0df32c715b752e
imalzy <imalzaya19@gmail.com>
2025-01-06 12:02:52 +0700

    fix: error

--- components/shared/Rating.tsx
+++ components/shared/Rating.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 type TRatingProps = {
-	rating: number;
-	className?: string;
+    rating: number | null;
+    className?: string;
 };
 
 export default function Rating({ rating, className }: TRatingProps) {
-	const roundedRating = Math.round(rating);
+    const roundedRating = rating !== null ? Math.round(rating) : 0;
 
-	return (
-		<div className={`stars d-flex items-center gap-1 ${className ?? className}`}>
-			{[...Array(5)].map((_, index) => (
-				<React.Fragment key={index}>
-					{index + 1 <= roundedRating ? (
-						<i className={`text-primary-dark fs-lg ti ti-star-filled`}></i>
-					) : (
-						<i className={`text-primary-dark fs-lg ti ti-star opacity-50`}></i>
-					)}
-				</React.Fragment>
-			))}
-		</div>
-	);
+    return (
+        <div className={`stars d-flex items-center gap-1 ${className ?? className}`}>
+            {[...Array(5)].map((_, index) => (
+                <React.Fragment key={index}>
+                    {index + 1 <= roundedRating ? (
+                        <i className={`text-primary-dark fs-lg ti ti-star-filled`}></i>
+                    ) : (
+                        <i className={`text-primary-dark fs-lg ti ti-star opacity-50`}></i>
+                    )}
+                </React.Fragment>
+            ))}
+        </div>
+    );
 }
