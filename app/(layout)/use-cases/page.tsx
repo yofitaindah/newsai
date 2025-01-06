bbcadbc15c5998739b8e71aca48f6d5fef346280
imalzy <imalzaya19@gmail.com>
2025-01-06 07:59:32 +0700

    feat: update

--- app/(layout)/use-cases/page.tsx
+++ app/(layout)/use-cases/page.tsx
@@ -6,46 +6,47 @@ import Reveal from "@/components/utils/Reveal";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Usecases",
+    title: "Usecases",
 };
 
 export default function UseCases() {
-	return (
-		<main className="flex-grow-1">
-			<Breadcrumb
-				title="Use Cases"
-				path={[
-					{
-						text: "Home",
-						link: "/",
-					},
-					{
-						text: "Use Cases",
-					},
-				]}
-			/>
+    return (
+        <main className="flex-grow-1">
+            <Breadcrumb
+                title="Use Cases"
+                path={[
+                    {
+                        text: "Home",
+                        link: "/",
+                    },
+                    {
+                        text: "Use Cases",
+                    },
+                ]}
+            />
 
-			<section className="pt-20 pb-10 pt-lg-30 pb-lg-15">
-				<div className="container">
-					<div className="row justify-center mb-18">
-						<div className="col-lg-9">
-							<div className="text-center">
-								<Reveal el="p" delay={0.05} className="text-primary-dark">
-									NewsAI Features
-								</Reveal>
-								<Reveal el="h1" className="text-white mb-0" delay={0.1}>
-								Read News Better and Faster, The Future of AI News is Already Here
-								</Reveal>
-							</div>
-						</div>
-					</div>
-					<div className="row justify-center row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gx-8 gy-14">
-						<UsecaseSection />
-					</div>
-				</div>
-			</section>
+            <section className="pt-20 pb-10 pt-lg-30 pb-lg-15">
+                <div className="container">
+                    <div className="row justify-center mb-18">
+                        <div className="col-lg-9">
+                            <div className="text-center">
+                                <Reveal el="p" delay={0.05} className="text-primary-dark">
+                                    NewsAI Features
+                                </Reveal>
+                                <Reveal el="h1" className="text-white mb-0" delay={0.1}>
+                                    Read News Better and Faster, The Future of AI News is Already
+                                    Here
+                                </Reveal>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row justify-center row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gx-8 gy-14">
+                        <UsecaseSection />
+                    </div>
+                </div>
+            </section>
 
-			<CtaHome />
-		</main>
-	);
+            <CtaHome />
+        </main>
+    );
 }
--- components/select/MultipleSelect.tsx
+++ components/select/MultipleSelect.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+import { useSearchParams } from "next/navigation";
 
 import { INewsCategory } from "@/types/categorynews";
 import { CATEGORIES_NEWS } from "@/data/categorynews";
@@ -8,6 +10,9 @@ import Icons from "../icon/icon";
 import useOutsideClick from "@/hook/useOutsideClick";
 
 const MultipleSelect = (): JSX.Element => {
+    const searchParams = useSearchParams();
+    const paramsFlag = searchParams.get("controlNews");
+    const [controlNews, setControlNews] = useState(paramsFlag === "true" ? true : false);
     const [query, setQuery] = useState("");
     const [selected, setSelected] = useState<string[]>([]);
     const [menuOpen, setMenuOpen] = useState(false);
@@ -15,11 +20,6 @@ const MultipleSelect = (): JSX.Element => {
     const inputRef = useRef<HTMLInputElement | null>(null);
 
     const ref = useRef<HTMLDivElement | null>(null);
-
-    const handleOutSideClick = () => {
-        console.log("menuOpen", menuOpen);
-        setMenuOpen(false);
-    };
 
     useOutsideClick(ref, () => {
         if (menuOpen) setMenuOpen(false); // Close the dropdown if it's open
@@ -42,142 +42,178 @@ const MultipleSelect = (): JSX.Element => {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                schedule: "* * * * *", // every 30 minute
+                schedule: "*/15 * * * *", // every 30 minute
                 query: selected.toString(),
                 // statusTrigger,
             }), // every minute
         });
     };
 
+    const handleStopCron = async () => {
+        await fetch("/api/cron/stop", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({}), // every minute
+        });
+    };
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            window.localStorage.setItem("controlNews", controlNews ? "true" : "false");
+        }
+    }, []);
+
     return (
         <div className="d-flex align-items-center gap-2 mb-5">
-            <div className="position-relative" style={{ width: "20rem", fontSize: "0.875rem" }}>
-                {selected?.length ? (
+            {controlNews && (
+                <>
                     <div
-                        className="position-relative card d-flex flex-wrap gap-1 p-2 mb-2"
-                        style={{ fontSize: "0.875rem", width: "20rem" }}
+                        className="position-relative"
+                        style={{ width: "20rem", fontSize: "0.875rem" }}
                     >
-                        {selected.map((tag, i) => {
-                            return (
-                                <div
-                                    key={i}
-                                    className="card-tag py-1.5 px-3 d-flex align-items-center gap-2"
-                                >
-                                    {tag}
-                                    <div
-                                        onMouseDown={(e) => e.preventDefault()}
-                                        onClick={() =>
-                                            setSelected(selected.filter((i) => i !== tag))
-                                        }
-                                    >
-                                        <Icons.Close />
-                                    </div>
-                                </div>
-                            );
-                        })}
-                        <div className="w-full text-right">
-                            <span
-                                className="text-gray-400 cursor-pointer"
-                                style={{cursor: 'pointer'}}
-                                onClick={() => {
-                                    setSelected([]);
-                                    inputRef.current?.focus();
-                                }}
+                        {selected?.length ? (
+                            <div
+                                className="position-relative card d-flex flex-wrap gap-1 p-2 mb-2"
+                                style={{ fontSize: "0.875rem", width: "20rem" }}
                             >
-                                Clear all
-                            </span>
-                        </div>
-                    </div>
-                ) : null}
-                <div
-                    className="card d-flex align-items-center flex-row p-3 gap-2"
-                    style={{ width: "20rem" }}
-                >
-                    <Icons.Search />
-                    <input
-                        ref={inputRef}
-                        type="text"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value.trimStart())}
-                        placeholder="Search or Create tags"
-                        className="bg-transparent text-sm flex-1 caret-rose-600"
-                        style={{
-                            fontSize: "0.875rem",
-                            caretColor: "#f43f5e",
-                            outline: "none",
-                            border: "none",
-                            width: "100%",
-                        }}
-                        onFocus={() => {
-                            setMenuOpen(true);
-                        }}
-                        onBlur={() => {
-                            setMenuOpen(true);
-                        }}
-                        onKeyDown={(e) => {
-                            if (e.key === "Enter" && !isDisable) {
-                                setSelected((prev) => [...prev, query]);
-                                setQuery("");
-                                setMenuOpen(true);
-                            }
-                        }}
-                    />
-                </div>
-
-                {menuOpen ? (
-                    <div
-                        ref={ref}
-                        className="card mt-2 p-1"
-                        style={{
-                            position: "absolute",
-                            maxHeight: "200px",
-                            display: "flex",
-                            overflowY: "auto",
-                            scrollbarWidth: "thin",
-                            scrollBehavior: "smooth",
-                            width: "100%",
-                            zIndex: 100,
-                        }}
-                    >
-                        <ul className="w-full">
-                            {filteredTags?.length ? (
-                                filteredTags.map((tag, i) => (
-                                    <li
-                                        key={i}
-                                        className="p-2 cursor-pointer hover:bg-rose-50 hover:text-rose-500 rounded-md w-full"
-                                        onMouseDown={(e) => e.preventDefault()}
+                                {selected.map((tag, i) => {
+                                    return (
+                                        <div
+                                            key={i}
+                                            className="card-tag py-1.5 px-3 d-flex align-items-center gap-2"
+                                        >
+                                            {tag}
+                                            <div
+                                                onMouseDown={(e) => e.preventDefault()}
+                                                onClick={() =>
+                                                    setSelected(selected.filter((i) => i !== tag))
+                                                }
+                                            >
+                                                <Icons.Close />
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                                <div className="w-full text-right">
+                                    <span
+                                        className="text-gray-400 cursor-pointer"
+                                        style={{ cursor: "pointer" }}
                                         onClick={() => {
-                                            setMenuOpen(true);
-                                            if (selected.length < 3) {
-                                                setSelected((prev: string[]) => [
-                                                    ...prev,
-                                                    tag.categoryName?.toString() ?? "",
-                                                ]);
-                                            } else {
-                                                alert("You can add only 3 tags");
-                                            }
-                                            setQuery("");
+                                            setSelected([]);
+                                            inputRef.current?.focus();
                                         }}
                                     >
-                                        {tag.categoryName}
-                                    </li>
-                                ))
-                            ) : (
-                                <li className="p-2 text-gray-500">No options available</li>
-                            )}
-                        </ul>
-                    </div>
-                ) : null}
-            </div>
+                                        Clear all
+                                    </span>
+                                </div>
+                            </div>
+                        ) : null}
+                        <div
+                            className="card d-flex align-items-center flex-row p-3 gap-2"
+                            style={{ width: "20rem" }}
+                        >
+                            <Icons.Search />
+                            <input
+                                ref={inputRef}
+                                type="text"
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value.trimStart())}
+                                placeholder="Search or Create tags"
+                                className="bg-transparent text-sm flex-1 caret-rose-600"
+                                style={{
+                                    fontSize: "0.875rem",
+                                    caretColor: "#f43f5e",
+                                    outline: "none",
+                                    border: "none",
+                                    width: "100%",
+                                }}
+                                onFocus={() => {
+                                    setMenuOpen(true);
+                                }}
+                                onBlur={() => {
+                                    setMenuOpen(true);
+                                }}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter" && !isDisable) {
+                                        setSelected((prev) => [...prev, query]);
+                                        setQuery("");
+                                        setMenuOpen(true);
+                                    }
+                                }}
+                            />
+                        </div>
 
-            <button
-                className="btn btn-primary-dark"
-                style={{ margin: "0px !important", placeSelf: "flex-end" }}
-                type="button"
-                onClick={handleUpdateNews}
-            >
-                Update
-            </button>
+                        {menuOpen ? (
+                            <div
+                                ref={ref}
+                                className="card mt-2 p-1"
+                                style={{
+                                    position: "absolute",
+                                    maxHeight: "200px",
+                                    display: "flex",
+                                    overflowY: "auto",
+                                    scrollbarWidth: "thin",
+                                    scrollBehavior: "smooth",
+                                    width: "100%",
+                                    zIndex: 100,
+                                }}
+                            >
+                                <ul className="w-full">
+                                    {filteredTags?.length ? (
+                                        filteredTags.map((tag, i) => (
+                                            <li
+                                                key={i}
+                                                className="p-2 cursor-pointer hover:bg-rose-50 hover:text-rose-500 rounded-md w-full"
+                                                onMouseDown={(e) => e.preventDefault()}
+                                                onClick={() => {
+                                                    setMenuOpen(true);
+                                                    if (selected.length < 3) {
+                                                        setSelected((prev: string[]) => [
+                                                            ...prev,
+                                                            tag.categoryName?.toString() ?? "",
+                                                        ]);
+                                                    } else {
+                                                        alert("You can add only 3 tags");
+                                                    }
+                                                    setQuery("");
+                                                }}
+                                            >
+                                                {tag.categoryName}
+                                            </li>
+                                        ))
+                                    ) : (
+                                        <li className="p-2 text-gray-500">No options available</li>
+                                    )}
+                                </ul>
+                            </div>
+                        ) : null}
+                    </div>
+
+                    <button
+                        className="btn btn-primary-dark"
+                        style={{ margin: "0px !important", placeSelf: "flex-end" }}
+                        type="button"
+                        onClick={handleUpdateNews}
+                    >
+                        Update
+                    </button>
+
+                    <button
+                        className="btn btn-primary-dark"
+                        style={{ margin: "0px !important", placeSelf: "flex-end" }}
+                        type="button"
+                        onClick={handleStopCron}
+                    >
+                        Stop
+                    </button>
+                </>
+            )}
+
+            {!controlNews && (
+                <>
+                    <span></span>
+                </>
+            )}
         </div>
     );
 };
